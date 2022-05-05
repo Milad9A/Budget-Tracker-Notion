a57e273725d8cf1e547dfbc2d500781278f3d0c8
Milad <millad.99.awad@gmail.com>
2022-05-05 13:55:49 +0300

    Add budget screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
+import 'package:budget_tracker_notion_ng/budget_repository.dart';
+import 'package:budget_tracker_notion_ng/failure_model.dart';
+import 'package:budget_tracker_notion_ng/item_model.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:intl/intl.dart';
 
 void main() async {
   await dotenv.load(fileName: '.env');
@@ -15,7 +19,7 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Notion Budget Tracker',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primaryColor: Colors.white,
+        appBarTheme: const AppBarTheme(color: Colors.white),
       ),
       home: const BudgetScreen(),
     );
@@ -30,8 +34,82 @@ class BudgetScreen extends StatefulWidget {
 }
 
 class _BudgetScreenState extends State<BudgetScreen> {
+  late Future<List<Item>> _futureItems;
+
+  @override
+  void initState() {
+    _futureItems = BudgetRepository().getItems();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title:
+            const Text('Budget Tracker', style: TextStyle(color: Colors.black)),
+      ),
+      body: FutureBuilder(
+        future: _futureItems,
+        builder: (BuildContext context, AsyncSnapshot snapshot) {
+          if (snapshot.hasData) {
+            // Show pie chart and list view of items.
+            final items = snapshot.data!;
+
+            return ListView.builder(
+              itemCount: items.length,
+              itemBuilder: (BuildContext context, int index) {
+                final item = items[index];
+                return Container(
+                  margin: const EdgeInsets.all(8.0),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(10.0),
+                    border: Border.all(
+                      width: 2.0,
+                      color: getCategoryColor(item.category),
+                    ),
+                    boxShadow: const [
+                      BoxShadow(
+                        color: Colors.black26,
+                        offset: Offset(0, 2),
+                        blurRadius: 6.0,
+                      )
+                    ],
+                  ),
+                  child: ListTile(
+                    title: Text(item.name),
+                    subtitle: Text(
+                        '${item.category} • ${DateFormat.yMd().format(item.date)}'),
+                    trailing: Text('-\$${item.price.toStringAsFixed(2)}'),
+                  ),
+                );
+              },
+            );
+          } else if (snapshot.hasError) {
+            // Show failure error message.
+            final failure = snapshot.error as Failure;
+            return Center(child: Text(failure.message));
+          }
+          // Show a loading spinner.
+          return const Center(child: CircularProgressIndicator());
+        },
+      ),
+    );
+  }
+}
+
+Color getCategoryColor(String category) {
+  switch (category) {
+    case 'Entertainment':
+      return Colors.red[400]!;
+    case 'Food':
+      return Colors.green[400]!;
+    case 'Personal':
+      return Colors.blue[400]!;
+    case 'Transportation':
+      return Colors.orange[400]!;
+    default:
+      return Colors.orange[400]!;
   }
 }
